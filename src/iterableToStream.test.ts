0cea6b9b4ac6328bac5d1ba0352edd105d9ef68a
Jason Dent <Jason3S@users.noreply.github.com>
2021-05-11 17:22:06 +0200

    feat: ignore empty values returned by the iterable. (#8)
    * feat: ignore empty values returned by the iterable.
    
    * ci: add github workflows
    
    * ci: make sure the original behavior is preserved

--- src/iterableToStream.test.ts
+++ src/iterableToStream.test.ts
@@ -1,13 +1,13 @@
 import { expect } from 'chai';
 import { Writable, WritableOptions } from 'stream';
-import * as loremIpsum from 'lorem-ipsum';
+import { loremIpsum } from 'lorem-ipsum';
 
 import iterableToStream from './iterableToStream';
 import { EventEmitter } from 'events';
 
 describe('Validate iterableToStream', () => {
     it('Test creating a stream from an array', async () => {
-        const target = new WritableDataStream()
+        const target = new WritableDataStream();
         const document = loremIpsum({ count: 100, units: 'paragraphs' });
         const words = document.split(/\b(?=\s)/g);
         const targetStream = iterableToStream(words).pipe(target);
@@ -15,25 +15,41 @@ describe('Validate iterableToStream', () => {
         expect(target.data.join('')).to.be.equal(document);
     });
 
-    it('Test creating a stream from an array', async () => {
-        const target = new WritableDataStream()
+    it('Test creating a stream from an array words', async () => {
+        const target = new WritableDataStream();
         const document = loremIpsum({ count: 100, units: 'words' });
         const words = iterateWords(document, '.done.');
         const targetStream = iterableToStream(words).pipe(target);
         await streamToPromise(targetStream);
         expect(target.data.join('')).to.be.equal(document + '.done.');
     });
+
+    it('tests that empty values do not break the steam', async () => {
+        const target = new WritableDataStream();
+        const data = ['One', 'Two', 'Three', '', null, undefined, 'buffer', Buffer.from('', 'utf-8'), 'Last'];
+        const targetStream = iterableToStream(data as string[], { encoding: 'utf8', removeEmpty: true }).pipe(target);
+        await streamToPromise(targetStream);
+        expect(target.data.join('')).to.be.equal(data.filter((a) => !!a).join(''));
+    });
+
+    it('tests that empty values stop the stream', async () => {
+        const target = new WritableDataStream();
+        const data = ['One', 'Two', 'Three', '', null, undefined, 'buffer', 'Last'];
+        const targetStream = iterableToStream(data as string[], { encoding: 'utf8', removeEmpty: false }).pipe(target);
+        await streamToPromise(targetStream);
+        expect(target.data.join('')).to.be.equal(['One', 'Two', 'Three'].join(''));
+    });
 });
 
-function *iterateWords(doc: string, final: string) {
+function* iterateWords(doc: string, final: string) {
     const words = doc.split(/\b(?=\s)/g);
-    yield *words;
+    yield* words;
     return final;
 }
 
 class WritableDataStream extends Writable {
     private _data: string[] = [];
-    static readonly  defaultOptions = { decodeStrings: false, highWaterMark: 64 };
+    static readonly defaultOptions = { decodeStrings: false, highWaterMark: 64 };
 
     constructor(options: WritableOptions = WritableDataStream.defaultOptions) {
         super({ ...WritableDataStream.defaultOptions, ...options });
@@ -50,7 +66,9 @@ class WritableDataStream extends Writable {
         callback();
     }
 
-    get data() { return this._data; }
+    get data() {
+        return this._data;
+    }
 }
 
 function streamToPromise(stream: EventEmitter): Promise<void> {
@@ -99,5 +117,3 @@ function streamToPromise(stream: EventEmitter): Promise<void> {
         }
     });
 }
-
-
--- src/iterableToStream.ts
+++ src/iterableToStream.ts
@@ -6,25 +6,33 @@ export type Streamable = string | Buffer;
 
 export type IterableLike<T> = Iterable<T> | IterableIterator<T>;
 
+interface ReadableOptions {
+    highWaterMark?: number;
+    encoding?: string | 'ascii' | 'utf8' | 'utf-8' | 'utf16le' | 'ucs2' | 'ucs-2' | 'base64' | 'latin1' | 'binary' | 'hex';
+    objectMode?: boolean;
+    autoDestroy?: boolean;
+}
+
+export interface Options extends ReadableOptions {
+    removeEmpty?: boolean;
+}
+
 /**
  * Transform an iterable into a node readable stream.
  */
-export function iterableToStream<T extends Streamable>(
-    src: IterableLike<T>,
-    options: stream.ReadableOptions = { encoding: 'utf8' }
-): stream.Readable {
+export function iterableToStream<T extends Streamable>(src: IterableLike<T>): stream.Readable;
+export function iterableToStream<T extends Streamable, U extends Options = Options>(src: IterableLike<T>, options: U): stream.Readable;
+export function iterableToStream<T extends Streamable>(src: IterableLike<T>, options: Options = { encoding: 'utf8' }): stream.Readable {
     return new ReadableObservableStream(src, options);
 }
 
 class ReadableObservableStream<T> extends stream.Readable {
+    private iter: Iterator<T, null | undefined | T>;
+    private removeEmpty: boolean;
 
-    private iter: Iterator<T>;
-
-    constructor(
-        private _source: IterableLike<T>,
-        options: stream.ReadableOptions,
-    ) {
+    constructor(private _source: IterableLike<T>, options: Options) {
         super(options);
+        this.removeEmpty = options.removeEmpty ?? false;
     }
 
     _read() {
@@ -32,16 +40,34 @@ class ReadableObservableStream<T> extends stream.Readable {
             this.iter = this._source[Symbol.iterator]();
         }
         let r = this.iter.next();
-        while (!r.done && this.push(r.value)) {
+        while (!r.done && this.pushValue(r.value)) {
             r = this.iter.next();
         }
         if (r.done) {
             // since it is possible for r.value to have something meaningful, we must check.
-            if (r.value !== null && r.value !== undefined) {
-                this.push(r.value);
+            if (r.value) {
+                this.pushValue(r.value);
             }
             this.push(null);
         }
+    }
+
+    /**
+     * Push values into the stream iff they are not empty.
+     * @param v value to push if possible.
+     * @returns true if more values can be pushed.
+     */
+    private pushValue(v: T | null | undefined): boolean {
+        if (!this.removeEmpty) {
+            return this.push(v);
+        }
+        return (
+            v === undefined ||
+            v === null ||
+            (typeof v === 'string' && v === '') ||
+            (Buffer.isBuffer(v) && v.byteLength === 0) ||
+            this.push(v)
+        );
     }
 }
 
