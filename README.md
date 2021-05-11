cbdde9273b4f087c95343f57c706ae8d7dd9d3dc
Jason Dent <Jason3S@users.noreply.github.com>
2021-05-11 20:05:05 +0200

    feat: use Node `stream.Readable.from` if it exists. (#11)
    * feat: use Node `stream.Readable.from` if it exists.
    
    * Update README.md

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # iterable-to-stream
+
 Convert iterable objects into node readable streams.
 
 ```typescript
@@ -7,3 +8,13 @@ import iterableToStream from 'iterable-to-stream';
 iterableToStream(['one', 'two', 'three']).pipe(process.stdout);
 ```
 
+## Note
+
+With version 10, Node introduced [stream.Readable.from](https://nodejs.org/dist/latest-v16.x/docs/api/stream.html#stream_stream_readable_from_iterable_options)
+which converts an iterator to a stream.
+
+This library will use the Node version if it is available.
+
+## Notice of Deprecation
+
+This library will be deprecated when Node 12 is deprecated.
--- src/iterableToStream.ts
+++ src/iterableToStream.ts
@@ -2,19 +2,17 @@ import * as stream from 'stream';
 
 // cspell:words streamable
 
-export type Streamable = string | Buffer;
+export type Streamable = string | Buffer | Object;
 
 export type IterableLike<T> = Iterable<T> | IterableIterator<T>;
 
-interface ReadableOptions {
-    highWaterMark?: number;
-    encoding?: string | 'ascii' | 'utf8' | 'utf-8' | 'utf16le' | 'ucs2' | 'ucs-2' | 'base64' | 'latin1' | 'binary' | 'hex';
-    objectMode?: boolean;
-    autoDestroy?: boolean;
-}
-
-export interface Options extends ReadableOptions {
+export interface Options extends stream.ReadableOptions {
     removeEmpty?: boolean;
+    /**
+     * Indicate if stream.Readable.from function should be used.
+     * @default true
+     */
+    _useNode?: boolean;
 }
 
 /**
@@ -23,16 +21,21 @@ export interface Options extends ReadableOptions {
 export function iterableToStream<T extends Streamable>(src: IterableLike<T>): stream.Readable;
 export function iterableToStream<T extends Streamable, U extends Options = Options>(src: IterableLike<T>, options: U): stream.Readable;
 export function iterableToStream<T extends Streamable>(src: IterableLike<T>, options: Options = { encoding: 'utf8' }): stream.Readable {
-    return new ReadableObservableStream(src, options);
+    const useNode = (options._useNode ?? true) && !!stream.Readable.from;
+    const objectMode = options.objectMode ?? useNode;
+    const filterFn = objectMode ? filterNullOrUndefined : filterEmptyStringOrBuffer;
+    const iter = options.removeEmpty ? filterFn(src) : src;
+    if (useNode) {
+        return stream.Readable.from(iter, options);
+    }
+    return new ReadableObservableStream(iter, options);
 }
 
 class ReadableObservableStream<T> extends stream.Readable {
     private iter: Iterator<T, null | undefined | T>;
-    private removeEmpty: boolean;
 
     constructor(private _source: IterableLike<T>, options: Options) {
         super(options);
-        this.removeEmpty = options.removeEmpty ?? false;
     }
 
     _read() {
@@ -40,34 +43,34 @@ class ReadableObservableStream<T> extends stream.Readable {
             this.iter = this._source[Symbol.iterator]();
         }
         let r = this.iter.next();
-        while (!r.done && this.pushValue(r.value)) {
+        while (!r.done && this.push(r.value)) {
             r = this.iter.next();
         }
         if (r.done) {
             // since it is possible for r.value to have something meaningful, we must check.
             if (r.value) {
-                this.pushValue(r.value);
+                this.push(r.value);
             }
             this.push(null);
         }
     }
+}
 
-    /**
-     * Push values into the stream iff they are not empty.
-     * @param v value to push if possible.
-     * @returns true if more values can be pushed.
-     */
-    private pushValue(v: T | null | undefined): boolean {
-        if (!this.removeEmpty) {
-            return this.push(v);
+function* filterEmptyStringOrBuffer<T>(src: IterableLike<T>) {
+    for (const v of src) {
+        if (v === undefined || v === null || (typeof v === 'string' && v === '') || (Buffer.isBuffer(v) && v.byteLength === 0)) {
+            continue;
         }
-        return (
-            v === undefined ||
-            v === null ||
-            (typeof v === 'string' && v === '') ||
-            (Buffer.isBuffer(v) && v.byteLength === 0) ||
-            this.push(v)
-        );
+        yield v;
+    }
+}
+
+function* filterNullOrUndefined<T>(src: IterableLike<T>) {
+    for (const v of src) {
+        if (v === undefined || v === null) {
+            continue;
+        }
+        yield v;
     }
 }
 
